c77678f4d115ee4938d84473242a264c997059e9
Anthonios Partheniou <partheniou@google.com>
2024-02-10 14:32:23 -0500

    refactor tests

--- setup.py
+++ setup.py
@@ -34,13 +34,6 @@ with io.open(readme_filename, encoding="utf-8") as readme_file:
     readme = readme_file.read()
 
 
-# Only include packages under the 'google' namespace. Do not include tests,
-# benchmarks, etc.
-packages = [
-    package for package in setuptools.find_packages() if package.startswith("google")
-]
-
-
 setuptools.setup(
     name=name,
     version=version,
